45871341e79c4500e1368015d6cd92f7c2a65494
Nitro <Nitro@LAPTOP-N9TP630A>
2021-10-21 21:43:49 +0200

    Přidejte soubory projektu.

---
+++ Hruby_Vojtech_E2C_ukol-podminka/Hruby_Vojtech_E2C_ukol-podminka.cpp
@@ -0,0 +1,18 @@
+﻿# include <stdio.h>
+
+int main(void) {
+
+	int x = 6;
+	int y = 5;
+
+	printf("cislo %d je ", x);
+
+	if (x < y) {
+		printf("mensi");
+	}
+	else {
+		printf("vetsi");
+	}
+
+	printf(" nez %d\n", y);
+}
